9899066e0f864f2320824809fe157505310526bf
guoanyi <2133292690@qq.com>
2025-09-22 23:41:09 +0800

    read_image

---
+++ basis/read_image/build/CMakeFiles/read_image.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/read_image.dir/src/main.cpp.o"
+  "CMakeFiles/read_image.dir/src/main.cpp.o.d"
+  "read_image"
+  "read_image.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/read_image.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
